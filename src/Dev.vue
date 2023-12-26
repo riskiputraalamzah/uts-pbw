76367113c8e363f41f18abcba6cdd13e8bd5d933
riskiputraalamzah <aryainggil3@gmail.com>
2023-12-27 02:00:20 +0700

    add overflow-hidden fixed when developement mode is published

--- src/Dev.vue
+++ src/Dev.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <h1
-      class="position-fixed top-0 bottom-0 start-0 end-0 bg-dark text-light text-center p-2 m-0 d-flex align-items-center justify-content-center lh-base"
+      class="position-fixed top-0 overlow-hidden bottom-0 start-0 end-0 bg-dark text-light text-center p-2 m-0 d-flex align-items-center justify-content-center lh-base"
     >
       Website sedang dalam tahap pengembangan<br />🤧😳🥵🥶
     </h1>
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+onMounted(() => {
+  document.body.classList.add('overflow-hidden')
+})
+</script>
